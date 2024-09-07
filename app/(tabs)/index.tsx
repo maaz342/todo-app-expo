950bfa950b8e2cd4f2bb92c6b272fa6cfd0d3132
maaz <maazkamran65@gmail.com>
2024-09-07 17:16:23 +0500

    todo-app-expo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,59 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { useState } from "react"
+import {Text,TextInput,View,Button, Alert} from "react-native"
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+  const [text,Settext]=useState("");
+  const [todo,SetTodo]=useState<any>([]);
+  const addTodo=()=>{
+
+    
+    console.log(text);
+    
+   // todo.push(text);
+    SetTodo([...todo,{text:text}]);
+    Settext("");
+    
+    
+
+  }
+  const deleteTodo=(i:any)=>{
+  todo.splice(i,1);
+  SetTodo([...todo])
+  }
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    <View style={{padding:50}}>
+      <Text style={{color:"green",textAlign:"center",fontSize:30}}>
+
+     Hello Expo
+     </Text>
+     <TextInput value={text}
+     onChange={(e:any)=>{
+      Settext(e.target.value);
+     }}
+     style={{width:"90%",borderWidth:2,borderColor:"green"}}
+     placeholder="Enter Todo"
+     />
+     {todo.map((todos:any,index:any)=>{
+return(
+  <Text key={index}>
+    {todos.text}
+    <Button onPress={()=>deleteTodo(index)} title="Delete" color="green">
+      </Button>
+
+  </Text>)})}
+
+
+    
+     <Button onPress={()=>addTodo()} title="Add" color="green">
+      
+     </Button>
+
+      </View>
+
+
+
+  )
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+
